8988fe2838328ab4592eb2ceec0a6131857ba47c
Atsu Dominic <dominic.atsu@princepsfinance.com>
2025-12-27 02:29:28 +0100

    Fix wayfinder import paths in login/register routes

--- resources/js/routes/login/index.ts
+++ resources/js/routes/login/index.ts
@@ -1,4 +1,4 @@
-import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../wayfinder'
+import { queryParams, type RouteQueryOptions, type RouteDefinition, type RouteFormDefinition } from './../../wayfinder'
 
 /**
  * @route '/login'
